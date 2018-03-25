5ec16cf869f116593acf61d2e0b98da36c156051
tsmi219 <tsmi219@lsu.edu>
2018-03-24 19:00:58 -0500

    foundation added and css

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+class CreatePostTestCase(TestCase):
+
+    def test_blog_not_authenicated(self):
+        client = Client()
+        client.logout()
+        url = reverse('post_new')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        #self.assertRedirects(response, "/accounts/login", status_code=302)
